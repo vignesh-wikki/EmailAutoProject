89d548583c8e6bd72419ab7d2ef6edbb2eaad10a
Vignesh <96247274+vignesh-wikki@users.noreply.github.com>
2024-11-18 09:18:38 +0530

    Update campaign.js

--- frontend/src/pages/campaign.js
+++ frontend/src/pages/campaign.js
@@ -22,7 +22,7 @@ const Campaign = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/campaigns");
+        const response = await fetch("https://email-auto-project-psi.vercel.app/api/campaigns");
         const data = await response.json();
         setCampaigns(data);
       } catch (error) {
