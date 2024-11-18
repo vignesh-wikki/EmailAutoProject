95eae3d99e3b5e73fcc89388f0362b2de71cb556
Vignesh <96247274+vignesh-wikki@users.noreply.github.com>
2024-11-18 06:47:48 +0530

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
  app.use(
    "/api", 
    createProxyMiddleware({
-     target: "https://email-auto-project-1lalmxwm8-vigneshwikkis-projects.vercel.app/",
+     target: "https://email-auto-project-1lalmxwm8-vigneshwikkis-projects.vercel.app",
      changeOrigin: true, 
    })
  );
